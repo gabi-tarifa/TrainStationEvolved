ce01db8f0fbce60a55f4c2642ac190f0f615ea47
gabi-tarifa <gabriel.tarifa.de.oliveira@gmail.com>
2025-12-17 19:49:38 -0300

    Complete login and sign up

--- TrainStationEvolved/app.py
+++ TrainStationEvolved/app.py
@@ -1,10 +1,11 @@
 import secrets
 from flask import Flask
-from flask import app, render_template, redirect, url_for, flash
+from flask import app, render_template, redirect, url_for, flash, jsonify, request
 from flask_login import current_user, login_required, LoginManager, login_user, logout_user
 from flask_cors import CORS
 from models import db, User
 import pymysql
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -52,6 +53,49 @@ def login_page():
 @app.route("/signup")
 def signup_page():
     return render_template("cadastro.html")
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    nick = data.get("nickname")
+    password = data.get("password")
+    email = data.get("email")
+    language = data.get("language")
+
+    if not nick and not password and not email and not language:
+        return jsonify({"message": "Por favor preencha todos os campos"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"message": "Email já cadastrado"}), 400
+    
+    pass_hash = generate_password_hash(password)
+
+    user = User(nickname=nick, password=pass_hash, email=email, language=language)
+
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify({"message": f"User {email} cadastrado com sucesso!"})
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email and not password:
+        return jsonify({"message":"Por favor preencha os dados corretamente!"}), 400
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user and check_password_hash(user.password, password):
+        login_user(user)
+
+        print("Usuario logado :) ")
+        
+        return jsonify({"message":"Login efetuado com sucesso!"}), 200
+    else:
+        return jsonify({"message":"Não foi possível encontrar o usuário"}), 401
 
 if __name__ == "__main__":
     app.run(debug=True)
--- TrainStationEvolved/models.py
+++ TrainStationEvolved/models.py
@@ -1,14 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "User"
 
-    id_usuario = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(20), nullable = False)
-    email = db.Column(db.String(50), nullable = False)
-    password = db.Column(db.String(24), nullable = False)
+    email = db.Column(db.String(50), nullable = False, unique=True)
+    password = db.Column(db.String(255), nullable = False)
     level = db.Column(db.Integer, default = 1)
     language = db.Column(db.String(4), nullable = False, default = "pt")
 
